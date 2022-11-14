23e24db06788ba96ec867578525391e83c327abb
jayesh sojitra <jayesh.sojitra103@gmail.com>
2022-11-14 19:46:43 +0530

    Updates

---
+++ precache-manifest.ad046976470ea88876005050b374862c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9798bd0bc39aca844a86a61b24172c7",
+    "url": "/use-react-form-hook/index.html"
+  },
+  {
+    "revision": "7b5439befffced3dd788",
+    "url": "/use-react-form-hook/static/css/main.c0be3dfb.chunk.css"
+  },
+  {
+    "revision": "5690a70793b49fe4383a",
+    "url": "/use-react-form-hook/static/js/2.1d65bfcb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-react-form-hook/static/js/2.1d65bfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5439befffced3dd788",
+    "url": "/use-react-form-hook/static/js/main.180ea188.chunk.js"
+  },
+  {
+    "revision": "b5b731fb57a7a28eadb3",
+    "url": "/use-react-form-hook/static/js/runtime-main.7c44174b.js"
+  }
+]);
